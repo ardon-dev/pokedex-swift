5f552b3690d945f24a5f283c4d9598ed07d303af
Ardón <josueardon.developer@gmail.com>
2025-01-26 18:29:37 -0600

    first commit

---
+++ Pokedex/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pokedex
+//
+//  Created by Josue on 26/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PokedexTests/PokedexTests.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexTests.swift
+//  PokedexTests
+//
+//  Created by Josue on 26/1/25.
+//
+
+import Testing
+@testable import Pokedex
+
+struct PokedexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
